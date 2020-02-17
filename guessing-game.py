34bc37a1a9f4e52a5d67f23548e480351a939190
Christopher Rogers <Christopher.Rogers1@marist.edu>
2020-02-17 17:37:39 -0500

    More usability improvements to the guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -8,7 +8,18 @@ def main():
         if guess.lower() == answer:
             zero += 1
             print("Correct guess, congratulations!")
-        elif guess.lower() == "quit":
+            ratCheck = 0
+            while ratCheck == 0:
+                response = input("Do you like this animal? Y/N: ")
+                if response[0].lower() == "y":
+                    print("I also like this animal")
+                    ratCheck+=1
+                elif response[0].lower() == "n":
+                    print("It's a shame you don't like this animal.")
+                    ratCheck+=1
+                else:
+                    print("Invalid answer, try again:")
+        elif guess[0].lower() == "q":
             zero += 1
         else:
             print("Incorrect guess, try again")
