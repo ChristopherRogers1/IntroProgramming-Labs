770058e3c89e5dfea1082a6bf077b7f8ffc2e07c
Christopher Rogers <Christopher.Rogers1@marist.edu>
2020-02-24 17:49:41 -0500

    Changed much of the program to methods

--- credentials.py
+++ credentials.py
@@ -2,19 +2,35 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Your Name Here
 # Created: YYYY-MM-DD
-def main():
-    # get user's first and last names
+
+def getName():
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-    # TODO modify this to generate a Marist-style username
-    uname = first + "." + last
-    # ask user to create a new password
-    passwd = input("Create a new password: ")
-    # TODO modify this to ensure the password has at least 8 characters
+    
+    return first, last
+
+def buildName(first, last):
+    return (first + "." + last)
+
+def checkStrength(passwd):
     while len(passwd) < 8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
     print("The force is strong in this one…")
+    return passwd
+
+def main():
+    # get user's first and last names
+    first, last = getName()
+    
+    # generate a Marist-style username
+    uname = buildName(first,last)
+    
+    # ask user to create a new password
+    passwd = input("Create a new password: ")
+    
+    # ensure the password has at least 8 characters
+    checkStrength(passwd)
     print("Account configured. Your new email address is",
     uname + "@marist.edu")
 main()
